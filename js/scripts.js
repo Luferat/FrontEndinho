c2300f27f7758c027bc11ea0bce464f59727543a
André Luferat <contato@luferat.net>
2024-01-24 11:45:08 -0300

    Ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
+app = {
+  'siteName': 'FrontEndinho',
+  'siteSlogan': 'É muito "inho" pro meu gosto!!!',
+}
+
 function loadFile(fileName, targetElement) {
   fetch(fileName)
-  .then(response => response.text())
-  .then(content => {
-    document.querySelector(targetElement).innerHTML = content;
-  })
+    .then(response => response.text())
+    .then(content => {
+      document.querySelector(targetElement).innerHTML = content;
+    })
 }
+
+function changeTitle(newTitle = '') {
+  if (newTitle == '') newTitle = app.siteSlogan;
+  document.querySelector('title').innerHTML = `${app.siteName} &centerdot;:&centerdot; ${newTitle}`;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  loadFile('_header.html', 'header');
+  loadFile('_nav.html', 'nav');
+  loadFile('_footer.html', 'footer');
+});
